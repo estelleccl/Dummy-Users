d0c8537d4ddbd1439efd344546fbe96851fe672b
estelle <estelleccl@gmail.com>
2014-10-27 16:33:55 +0000

    :)

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -21,7 +21,7 @@ post '/login' do
   @user = User.authenticate(params[:email], params[:password])
    if @user
      session[:username] = @user[:username]
-     redirect "/"
+     redirect '/'
    else
      erb :invalid
    end
@@ -33,23 +33,26 @@ get '/logout' do
 end
 
 get '/signup' do
-
   erb :signup
 end
 
 post '/signup' do
   @user = User.create(username: params[:username], email: params[:email], password: params[:password])
    if !@user.new_record? #Returns true if this object hasn’t been saved yet
-    session[:username] = params[:username]
-    redirect "/"
+      session[:username] = params[:username]
+      redirect "/"
   else
-     
-     @error_msg = @user.errors.full_messages
-     puts @error_msg
-     erb :signup
+      @error_msg = @user.errors.full_messages
+      erb :signup
   end
-
 end
 
+get '/secure' do 
+  if !login?
+    redirect "/"
+  else
+    erb :secure
+  end
+end
 
 
